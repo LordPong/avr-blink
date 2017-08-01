b7a20234476680e491adbd8de35bbebfa35fd0ef
Ulrich Peter Berg <Peter.Berg@rumrussen.de>
2017-08-01 16:03:19 +0200

    comments added

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
-#define F_CPU 1000000UL
+#define F_CPU 1000000UL		// set frequency (1000000Hz = 1MHz)
 
-#include <avr/io.h>
-#include <util/delay.h>
+#include <avr/io.h>			// including default io library
+#include <util/delay.h>		// including library for delay functions
 
 int main(void)
 {
-	DDRB |= (1<<PB0);
+	DDRB |= (1<<PB0);		// initialize PORTB0 as output
 	while(1)
 	{
-		PORTB ^= (1<<PB0);
-		_delay_ms(1000);
+		PORTB ^= (1<<PB0);	// toggle PORTB0
+		_delay_ms(1000);	// delay for 1000ms
 	}
 	return 0;
 }
